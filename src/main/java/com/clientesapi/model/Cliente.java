a83fc00c07221a72a56ae94db4d271c4787cd039
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-01-15 11:27:33 -0300

    Adicionando anotação do Lombok

--- src/main/java/com/clientesapi/model/Cliente.java
+++ src/main/java/com/clientesapi/model/Cliente.java
@@ -9,7 +9,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+
 @Entity
+@Data
 public class Cliente implements Serializable {
 
 	private static final long serialVersionUID = 1L;
