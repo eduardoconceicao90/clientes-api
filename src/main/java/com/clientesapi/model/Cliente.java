7b3ee142957ff13de3d2c8b7188c4b4499a13f5a
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-01-15 11:21:44 -0300

    Mapeando a entidade Cliente

---
+++ src/main/java/com/clientesapi/model/Cliente.java
@@ -0,0 +1,30 @@
+package com.clientesapi.model;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cliente implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, length = 150)
+	private String nome;
+	
+	@Column(nullable = false, length = 11)
+	private String cpf;
+	
+	@Column
+	private LocalDate dataCadastro;
+
+}
